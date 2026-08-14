2444da627e5f10420b51a68a1b44401573d07df4
alupx <235094323+alupx@users.noreply.github.com>
2026-08-14 23:26:22 +0200

    Add solution to challenge 7

---
+++ set1/challenge7.py
@@ -0,0 +1,15 @@
+import base64
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+with open("7.txt", "r") as file:
+    ctb64 = ""
+    for line in file:
+        ctb64 += line
+ct = base64.b64decode(ctb64)
+
+key = b"YELLOW SUBMARINE"
+
+cipher = Cipher(algorithms.AES(key), modes.ECB())
+decryptor = cipher.decryptor()
+print(decryptor.update(ct) + decryptor.finalize())
